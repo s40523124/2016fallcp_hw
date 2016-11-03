cfc17beb710efca7290a27e6ade98753ee34c968
s40523124 <40523124@gm.nfu.edu.tw>
2016-11-03 23:33:29 +0800

    Update w4.py

--- w4.py
+++ w4.py
@@ -1,9 +1,10 @@
+print(GGWP)
 '''
 tring = ("s40523124")
 mystring = mystring +  " test"
 print(mystring)
-'''
 
 thetext = input("Enter some text ")
 print ("This is what you entered" , end="")
 print (thetext)
+'''
