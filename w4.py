702090aa8cc725d731c82a5e37e220505625aaa2
s40523124 <40523124@gm.nfu.edu.tw>
2016-11-03 23:40:31 +0800

    Updated from Brython Server: 2016/11/3 下午11:40:32

--- w4.py
+++ w4.py
@@ -1,10 +1,8 @@
-print("GGWP")
 '''
 tring = ("s40523124")
 mystring = mystring +  " test"
 print(mystring)
-
+'''
 thetext = input("Enter some text ")
 print ("This is what you entered" , end="")
 print (thetext)
-'''
